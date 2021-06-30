f8b1ff74340a1c44d40ecd01c4e126559479e173
aeae_1@naver.com <aeae_1@naver.com>
2021-06-30 23:58:33 +0900

    add: 음악 재생 기능

--- main.py
+++ main.py
@@ -1,38 +1,68 @@
-from discord.ext.commands.converter import VoiceChannelConverter
 import discord
-from discord.ext import commands
+import youtube_dl
 
-bot = commands.Bot(command_prefix='!')
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
 
-token = "ODU5MDMwMDgxMTIwMzcwNjg5.YNmwGw.BFKJu3dEKRS3haIKnnsezHpnB9s"
+token = "ODU5MDMwMDgxMTIwMzcwNjg5.YNmwGw.smF05bVG4XJ9MGI0oXZNq3TefIE"
 
-@bot.event
+## 로그인
+@client.event
 async def on_ready():
     print('다음으로 로그인합니다: ')
-    print(bot.user.name)
+    print(client.user.name)
     game = discord.Game('봇이 활동중에 표시 될 이름')
-    await bot.change_presence(status=discord.Status.online, activity=None)
+    await client.change_presence(status=discord.Status.online, activity=None)
 
-@bot.command()
-async def 따라하기(ctx, *, text):
-    await ctx.send(embed = discord.Embed(title = '따라하기', description = text, color = 0x00ff00))
+@client.event
+async def on_message(message):
 
-@bot.command()
-async def 들어와(ctx):
-    try:
-        global vc
-        vc = await ctx.message.author.voice.channel.connect()
-    except:
+    ## !따라하기 <text>
+    if message.content.startswith("!따라하기"):
+        text = message.content.split(" ")[1]
+        await message.channel.send(embed = discord.Embed(title = '따라하기', description = text, color = 0x00ff00))
+
+    ## !입장
+    if message.content.startswith("!입장"):
         try:
-            await vc.move_to(ctx.message.author.voice.channel)
+            global vc
+            vc = await message.author.voice.channel.connect()
+            await message.channel.send("보이스채널에 입장합니다.")
         except:
-            await ctx.send("채널에 유저가 접속해있지 않네요..")
+            try:
+                await vc.move_to(message.author.voice.channel)
+            except:
+                await message.channel.send("채널에 유저가 접속해있지 않네요..")
 
-@bot.command()
-async def 나가(ctx):
-    try:
-        await vc.disconnect()
-    except:
-        await ctx.send("이미 그 채널에 속해있지 않아요.")
+    ## !퇴장
+    if message.content.startswith("!퇴장"):
+        for vc in client.voice_clients:
+            if vc.guild == message.guild:
+                voice = vc
 
-bot.run(token)
+        try:
+            await voice.disconnect()
+            await message.channel.send("보이스채널에서 퇴장합니다.")
+        except:
+            await message.channel.send("이미 그 채널에 속해있지 않아요.")
+
+    ## !재생 <url>
+    if message.content.startswith("!재생"):
+        for vc in client.voice_clients:
+            if vc.guild == message.guild:
+                voice = vc
+        
+        url = message.content.split(" ")[1]
+        option = {'format': 'bestaudio'}
+        FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+
+        with youtube_dl.YoutubeDL(option) as ydl:
+            info = ydl.extract_info(url, download=False)
+            mTitle = info['title']
+            mUrl = info['formats'][0]['url']
+        
+        voice.play(discord.FFmpegPCMAudio(mUrl, **FFMPEG_OPTIONS))
+        await message.channel.send(mTitle + "을 재생합니다.")
+
+
+client.run(token)
